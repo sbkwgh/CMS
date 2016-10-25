9bd61cd9266a3de79cf5cea28232b854f6e29582
sbkwgh <sbkwgh@gmail.com>
2016-10-25 21:11:33 +0100

    Generate authorId on saving user, at findPostsByUser static method

--- models/user.js
+++ models/user.js
@@ -1,14 +1,27 @@
 var mongoose = require('mongoose');
+var Post = require('./post.js');
 var hashGen = require('password-hash-and-salt');
+var shortid = require('shortid');
 
 var userSchema = mongoose.Schema({
 	username: {type: String, required: true},
 	author: {type: String, required: true},
-	hash: {type: String, required: true},
+	authorId: String,
+	hash: {type: String, required: true}
 });
+
+userSchema.options.toJSON = {
+	transform: function(doc, ret, options) {
+		delete ret.hash;
+		return ret;
+	}
+};
 
 userSchema.pre('save', function(next) {
 	var self = this;
+
+	this.authorId = author.replace(/\s/g, '-') + '-' + shortid.generate();
+
 	hashGen(this.hash).hash(function(err, hash) {
 		if(err) {
 			self.invalidate('user', err)
@@ -19,6 +32,28 @@ userSchema.pre('save', function(next) {
 		}
 	});
 });
+
+userSchema.statics.findPostsByUser = function(authorId, cb) {
+	User.findOne({authorId: authorId}, function(err, user) {
+		if(err) {
+			cb(err);
+		} else if(!user) {
+			cb(null, {})
+		} else {
+			var JSONUser = user.toJSON();
+
+			Post.find({authorId: user.authorId}, function(err, posts) {
+				if(err) {
+					cb(err);
+				} else {
+					JSONUser.posts = posts.map( p => p.toJSON({virtuals: true}) );
+
+					cb(null, JSONUser);
+				}
+			});
+		}
+	});
+};
 
 userSchema.statics.findUserAndCompareHashes = function(username, password, cb) {
 	if(typeof username !== 'string' && typeof password !== 'string') {
